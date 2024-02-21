9ac8592bb7c17558008cceeee3eea9a4c452c85c
Shashank09012003 <156293289+Shashank09012003@users.noreply.github.com>
2024-02-21 21:27:22 +0530

    ok

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:shashanksharma7581@gmail.com">shashanksharma7581@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/shashank-sharma-8326b4278/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">SHASHANK SHARMA</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Shashank09012003">SHASHANK SHARMA</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
